db1302d2ae5d345a42c2cb7109c980eadd68fb8e
william <willshinji@gmail.com>
2018-10-31 14:20:44 -0200

    unique email address

---
+++ challenges/unique_email_address.js
@@ -0,0 +1,43 @@
+let emails = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
+
+
+const compose = (fx, fy) => {
+  return v => {
+    return fx(fy(v))
+  }
+}
+const removeDot = string => string.replace(/\./g, '')
+
+const ignoreEverythingAfterPlus = string => {
+  const [aux] = string.split('+');
+  return aux;
+}
+const validEmail = compose(ignoreEverythingAfterPlus, removeDot, formatedEmail)
+
+const formatedEmail = (name, domain) => `${name}@${domain}` 
+
+const onlyUnique = (value, index, self) => { 
+    return self.indexOf(value) === index;
+}
+
+function parameterize(email){ 
+  let parts = email.split('@')
+  let [name, domain] = parts 
+  let cleanName = validEmail(name)
+  return formatedEmail(cleanName, domain);
+}
+
+
+
+const numUniqueEmails = function(emails) {
+  
+  const emailsParemeterize = emails.map( email => { 
+    return parameterize(email)
+  })
+
+  let unique = emailsParemeterize.filter(onlyUnique)
+
+  return unique.length
+};
+
+console.log(numUniqueEmails(emails))
