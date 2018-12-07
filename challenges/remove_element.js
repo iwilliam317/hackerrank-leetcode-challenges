b8c8166badee1d4b2484bff2d3665b77b1b73e8c
William Shinji <willshinji@gmail.com>
2018-12-07 00:11:04 -0200

    Update remove_element.js

--- challenges/remove_element.js
+++ challenges/remove_element.js
@@ -5,7 +5,7 @@ const removeElement = (nums, val) => {
     for(let i = 0; i < n; i++){
       if(nums[i] == val){
         nums.splice(i,1)
-        
+        i--;
       }
     }
     return nums.length
