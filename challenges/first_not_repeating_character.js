01989ab572b6e57078293e3b990944c98c8c66f6
william <willshinji@gmail.com>
2019-01-05 00:15:09 -0200

    First Not Repeating Character

---
+++ challenges/first_not_repeating_character.js
@@ -0,0 +1,12 @@
+// https://app.codesignal.com/interview-practice/task/uX5iLwhc6L5ckSyNC
+const amountOccurence = (letter, array) => array.filter(a => a == letter.toString()).length
+
+const firstNotRepeatingCharacter = s => {
+  let arr = s.split('')
+  for(let i = 0; i < arr.length; i++){
+    if(amountOccurence(arr[i], arr) == 1){
+      return arr[i]
+    }
+  }
+  return '_'
+}
