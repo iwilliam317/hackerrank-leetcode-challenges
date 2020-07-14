f6b90c910fd6fc2e9058093fa97f5a903d888f8c
Shinji Obana <willshinji@gmail.com>
2020-07-14 19:58:28 +0100

    fixing linked list exercises

--- hackerrank-challenges/linked-list/insert-a-node-at-the-tail-of-a-linked-list/insert_a_node_at_the_tail_of_a_linked_list.py
+++ hackerrank-challenges/linked-list/insert-a-node-at-the-tail-of-a-linked-list/insert_a_node_at_the_tail_of_a_linked_list.py
@@ -1,7 +1,6 @@
 # https://www.hackerrank.com/challenges/insert-a-node-at-the-tail-of-a-linked-list/problem
 
 
-
 #!/bin/python3
 
 import math
@@ -54,3 +53,18 @@ def insertNodeAtTail(head, data):
 
 
 if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
